a4fdb412106e125f28a5a28ecffba21a4f352493
Cyril234 <cyril_seitz@hotmail.com>
2025-12-09 10:22:05 +0100

    add signe up screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-
+import SignIn from "./components/signIn/signIn";
 import NavBar from "./components/navBar/navBar";
+import SignUp from "./components/signeUp/signeUp";
+import SignUpConfirmation from "./components/signUpConfirmation/signUpConfirmation";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <NavBar />
+       <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/signIn" element={<SignIn />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/signUpConfirmation" element={<SignUpConfirmation />} />
+       </Routes>
     </>
   );
 }
