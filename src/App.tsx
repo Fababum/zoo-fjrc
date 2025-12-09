e1eb7cc1dc204d79026e49437ada59cfa86b790e
Ryan Heatley <ryan.heatley@outlook.com>
2025-12-09 07:52:53 +0100

    finalisation of the Error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 
 import NavBar from "./components/navBar/navBar";
+import NotFound from "./components/errorpage/errorpage";
 
 function App() {
+  const path = window.location.pathname;
+  // List of known app routes (add more as your app grows)
+  const knownPaths = ['/', '/homepage', '/home', '/signin', '/signUp', '/signup', '/purchaseTickets', '/articles', '/map'];
+  const isKnown = knownPaths.includes(path.toLowerCase());
+
   return (
     <>
       <NavBar />
+      {!isKnown && <NotFound />}
     </>
   );
 }
