ff0538ef91f082171c566c4718a82226a8caaeb8
Fababum <fabian.spiri@gmx.ch>
2026-01-13 09:41:43 +0100

    chatbot and language functioning

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,41 @@ import NotFound from "./components/errorpage/errorpage";
 import SignIn from "./components/signIn/signIn";
 import SignUp from "./components/signeUp/signeUp";
 import SignUpConfirmation from "./components/signUpConfirmation/signUpConfirmation";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams, Navigate } from "react-router-dom";
 import MapPage from "./components/map/map";
 import Articles from "./components/articles/articles";
+import Chatbot from "./components/chatbot/Chatbot";
+import { useContext, useEffect } from "react";
+import { TranslationsContext } from "./components/TranslationsContext";
 
+function LanguageWrapper({ children }: { children: React.ReactNode }) {
+  const { lang } = useParams<{ lang: string }>();
+  const context = useContext(TranslationsContext);
+  
+  useEffect(() => {
+    if (lang && context && ['de', 'en', 'fr', 'it'].includes(lang)) {
+      context.setLang(lang);
+    }
+  }, [lang, context]);
+
+  return <>{children}</>;
+}
 
 function App() {
-  const path = window.location.pathname;
-  // List of known app routes (add more as your app grows)
-  const knownPaths = ['/', '/homepage', '/home', '/signin', '/signUp', '/signup', '/purchaseTickets', '/articles', '/map'];
-  const isKnown = knownPaths.includes(path.toLowerCase());
-
   return (
     <>
       <NavBar />
-      {!isKnown && <NotFound />}
        <Routes>
-            <Route path="/" element={<h1>Home</h1>} />
-            <Route path="/map" element={<MapPage />} />
-            <Route path="/signIn" element={<SignIn />} />
-            <Route path="/articles/:article" element={<Articles />} />
-            <Route path="/signUp" element={<SignUp />} />
-            <Route path="/signUpConfirmation" element={<SignUpConfirmation />} />
-            <Route path="/articles" element={<Articles />} />
+            <Route path="/" element={<Navigate to="/de" replace />} />
+            <Route path="/:lang" element={<LanguageWrapper><h1>Home</h1></LanguageWrapper>} />
+            <Route path="/:lang/map" element={<LanguageWrapper><MapPage /></LanguageWrapper>} />
+            <Route path="/:lang/signIn" element={<LanguageWrapper><SignIn /></LanguageWrapper>} />
+            <Route path="/:lang/articles/:article" element={<LanguageWrapper><Articles /></LanguageWrapper>} />
+            <Route path="/:lang/articles" element={<LanguageWrapper><Articles /></LanguageWrapper>} />
+            <Route path="/:lang/signUp" element={<LanguageWrapper><SignUp /></LanguageWrapper>} />
+            <Route path="/:lang/signUpConfirmation" element={<LanguageWrapper><SignUpConfirmation /></LanguageWrapper>} />
+            <Route path="/:lang/chatbot" element={<LanguageWrapper><Chatbot /></LanguageWrapper>} />
+            <Route path="*" element={<NotFound />} />
        </Routes>
     </>
   );
